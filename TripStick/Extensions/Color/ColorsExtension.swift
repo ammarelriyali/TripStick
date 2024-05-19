5ab824abf68276c1225f3283d0a25b54abf4a0be
Ammar Elriyali <ammarelriyali@gmail.com>
2024-05-19 22:19:00 +0300

    add Extensions

--- TripStick/Extensions/Color/ColorsExtension.swift
+++ TripStick/Extensions/Color/ColorsExtension.swift
@@ -10,5 +10,10 @@ import SwiftUI
 extension Color {
     struct theme {
         
+        static let background = Color("Background")
+        static let backgroundCard = Color("BackgroundCard")
+        static let highlight = Color("Highlight")
+        static let primary = Color("CustomPrimaryColor")
+        static let secondary = Color("CustomSecondaryColor")
     }
 }
