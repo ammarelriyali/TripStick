fbdf6733f71641c7d57aeb45c2b1e5890e1c607e
Ammar Elriyali <ammarelriyali@gmail.com>
2024-05-19 21:05:50 +0300

    add AppFiles

---
+++ TripStick/AppFiles/TripStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripStickApp.swift
+//  TripStick
+//
+//  Created by ammar on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TripStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
